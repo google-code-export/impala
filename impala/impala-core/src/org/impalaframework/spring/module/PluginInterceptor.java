96eeb3d4ff3b5b6766387892ea65ce7f4d7022e1
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-01-01 23:18:50 +0000

    Package rename

--- impala/impala-core/src/org/impalaframework/spring/module/PluginInterceptor.java
+++ impala/impala-core/src/org/impalaframework/spring/module/PluginInterceptor.java
@@ -12,7 +12,7 @@
  * the License.
  */
 
-package org.impalaframework.spring.plugin;
+package org.impalaframework.spring.module;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
